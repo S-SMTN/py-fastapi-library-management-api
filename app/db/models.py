c675af0b25365028a2b04ad27f5f6fd2543d4955
S-SMTN <cven28@gmail.com>
2024-09-16 00:57:07 +0300

    initiated db with models and migrations

---
+++ app/db/models.py
@@ -0,0 +1,27 @@
+from sqlalchemy import Column, Integer, String, Date, ForeignKey
+from sqlalchemy.orm import relationship
+
+from app.db.database import Base
+
+
+class DBBaseModel(Base):
+    __abstract__ = True
+    __allow_unmapped__ = True
+
+    id = Column(Integer, primary_key=True, index=True)
+
+
+class DBBook(DBBaseModel):
+    __tablename__ = "book"
+
+    title = Column(String(255))
+    summary = Column(String)
+    publication_date = Column(Date)
+    author_id = Column(Integer, ForeignKey("author.id"))
+
+
+class DBAuthor(DBBaseModel):
+    __tablename__ = "author"
+    name = Column(String(50), unique=True)
+    bio = Column(String(255))
+    books = relationship(DBBook, back_populates="author")
